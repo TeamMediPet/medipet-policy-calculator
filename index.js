6c9cdbde66ef61d674f8dd0378b948d98294ab1f
Erez Greenberg <erez@nona.digital>
2019-05-15 11:56:26 +0200

    chore: add primarypet function to index

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const calculate = require('./src/calculate')
 const validate = require('./src/validate')
+const pickAPrimary = require('./src/pickAPrimary')
 
 module.exports = {
   calculate,
   validate,
+  pickAPrimary,
 }
