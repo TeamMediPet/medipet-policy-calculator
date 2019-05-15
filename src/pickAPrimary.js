98e082eec8c0163c6cb9b5acf26bf38a04c736c3
Erez Greenberg <erez@nona.digital>
2019-05-15 12:58:22 +0200

    fix: primary pet logic

--- src/pickAPrimary.js
+++ src/pickAPrimary.js
@@ -1,14 +1,30 @@
-const isDog = pet => pet.petType === 'Dog'
-const isCat = pet => pet.petType === 'Cat'
+const isOption2Dog = pet => pet.petType === 'Dog' && pet.planType === 'option2'
+const isOption1Dog = pet => pet.petType === 'Dog' && pet.planType === 'option1'
+const isOption2Cat = pet => pet.petType === 'Cat' && pet.planType === 'option2'
+const isOption1Cat = pet => pet.petType === 'Cat' && pet.planType === 'option1'
 
-const firstDogOrCat = pets => {
-  const firstDog = pets.find(isDog)
-  return firstDog ? firstDog : pets.find(isCat)
+const mostExpensivePet = pets => {
+  let firstPet = null
+
+  // Look through dogs first...
+  firstPet = pets.find(isOption2Dog)
+  if (!firstPet) {
+    firstPet = pets.find(isOption1Dog)
+  }
+  // Then cats
+  if (!firstPet) {
+    firstPet = pets.find(isOption2Cat)
+  }
+  if (!firstPet) {
+    firstPet = pets.find(isOption1Cat)
+  }
+
+  return firstPet
 }
 
 const pickAPrimary = pets => {
   const eligible = pets.filter(pet => pet.planType !== 'accidentOnly')
-  const primaryPet = firstDogOrCat(eligible) || pets[0]
+  const primaryPet = mostExpensivePet(eligible) || pets[0]
 
   return {
     pets: pets.filter(pet => pet.id !== primaryPet.id),
