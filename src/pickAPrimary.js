3b42d49d6a41fab04a4010c27463b71d3a5ea693
Chace <chacestew@gmail.com>
2020-04-28 17:48:23 +0200

    update primary pet picker logic

--- src/pickAPrimary.js
+++ src/pickAPrimary.js
@@ -1,30 +1,42 @@
+const isOlderThanSix = require('./isOlderThanSix')
+
 const isOption2Dog = pet => pet.petType === 'Dog' && pet.planType === 'option2'
 const isOption1Dog = pet => pet.petType === 'Dog' && pet.planType === 'option1'
 const isOption2Cat = pet => pet.petType === 'Cat' && pet.planType === 'option2'
 const isOption1Cat = pet => pet.petType === 'Cat' && pet.planType === 'option1'
 
-const mostExpensivePet = pets => {
+const findPrimary = pets => {
   let firstPet = null
 
-  // Look through dogs first...
-  firstPet = pets.find(isOption2Dog)
-  if (!firstPet) {
-    firstPet = pets.find(isOption1Dog)
-  }
-  // Then cats
-  if (!firstPet) {
-    firstPet = pets.find(isOption2Cat)
-  }
-  if (!firstPet) {
-    firstPet = pets.find(isOption1Cat)
-  }
+  if (!pets.length) return firstPet
 
+  firstPet = pets.find(isOption2Dog)
+  if (firstPet) return firstPet
+
+  firstPet = pets.find(isOption1Dog)
+  if (firstPet) return firstPet
+
+  firstPet = pets.find(isOption2Cat)
+  if (firstPet) return firstPet
+
+  firstPet = pets.find(isOption1Cat)
   return firstPet
 }
 
 const pickAPrimary = pets => {
-  const eligible = pets.filter(pet => pet.planType !== 'accidentOnly')
-  const primaryPet = mostExpensivePet(eligible) || pets[0]
+  // Filter out accident/lite from prefered set
+  const eligible = pets.filter(
+    pet => pet.planType !== 'accidentOnly' || pet.planType !== 'lite',
+  )
+
+  // Filter dogs older than six from most preferred set
+  const youngerThanSix = eligible.filter(pet =>
+    pet.petType === 'Cat' ? true : !isOlderThanSix(pet),
+  )
+
+  // Choose, in order, most expensive from [young dogs, any age cats, no accident/lite], OR [any age, no accident/lite], OR [any age, any plan]
+  const primaryPet =
+    findPrimary(youngerThanSix) || findPrimary(eligible) || pets[0]
 
   return {
     pets: pets.filter(pet => pet.id !== primaryPet.id),
--- src/pickAPrimary.test.js
+++ src/pickAPrimary.test.js
@@ -6,17 +6,16 @@ describe('Pick a primary pet', () => {
       {
         planType: 'accidentOnly',
         petType: 'Cat',
+        dob: nYearsAgo(5),
       },
       {
         petType: 'Dog',
         planType: 'accidentOnly',
+        dob: nYearsAgo(5),
       },
     ]
 
-    expect(pickAPrimary(pets).primaryPet).toEqual({
-      planType: 'accidentOnly',
-      petType: 'Cat',
-    })
+    expect(pickAPrimary(pets).primaryPet).toEqual(pets[0])
   })
 
   test('It should pick the cat if the only dog is on accidently only', () => {
@@ -24,17 +23,16 @@ describe('Pick a primary pet', () => {
       {
         planType: 'option1',
         petType: 'Cat',
+        dob: nYearsAgo(5),
       },
       {
         petType: 'Dog',
         planType: 'accidentOnly',
+        dob: nYearsAgo(5),
       },
     ]
 
-    expect(pickAPrimary(pets).primaryPet).toEqual({
-      planType: 'option1',
-      petType: 'Cat',
-    })
+    expect(pickAPrimary(pets).primaryPet).toEqual(pets[0])
   })
 
   test('It should pick the dog if the both are eligible', () => {
@@ -42,39 +40,38 @@ describe('Pick a primary pet', () => {
       {
         planType: 'option1',
         petType: 'Cat',
+        dob: nYearsAgo(5),
       },
       {
         planType: 'option2',
         petType: 'Cat',
+        dob: nYearsAgo(5),
       },
       {
         petType: 'Dog',
         planType: 'option1',
+        dob: nYearsAgo(5),
       },
     ]
 
-    expect(pickAPrimary(pets).primaryPet).toEqual({
-      planType: 'option1',
-      petType: 'Dog',
-    })
+    expect(pickAPrimary(pets).primaryPet).toEqual(pets[2])
   })
 
-  test('It should pick the dog of option2 if the both are eligible', () => {
+  test('It should pick the dog of option2 if both are eligible', () => {
     const pets = [
       {
         planType: 'option1',
         petType: 'Dog',
+        dob: nYearsAgo(5),
       },
       {
         petType: 'Dog',
         planType: 'option2',
+        dob: nYearsAgo(5),
       },
     ]
 
-    expect(pickAPrimary(pets).primaryPet).toEqual({
-      planType: 'option2',
-      petType: 'Dog',
-    })
+    expect(pickAPrimary(pets).primaryPet).toEqual(pets[1])
   })
 
   test('It should pick the cat of option2 if the both are eligible', () => {
@@ -82,20 +79,61 @@ describe('Pick a primary pet', () => {
       {
         planType: 'option1',
         petType: 'Cat',
+        dob: nYearsAgo(5),
       },
       {
         petType: 'Cat',
         planType: 'option2',
+        dob: nYearsAgo(5),
       },
       {
         petType: 'Dog',
         planType: 'accidentOnly',
+        dob: nYearsAgo(5),
       },
     ]
 
-    expect(pickAPrimary(pets).primaryPet).toEqual({
-      planType: 'option2',
-      petType: 'Cat',
-    })
+    expect(pickAPrimary(pets).primaryPet).toEqual(pets[1])
+  })
+
+  const nYearsAgo = n => new Date().setFullYear(new Date().getFullYear() - n)
+
+  test('It should pick the younger dog if the other is over 6', () => {
+    const pets = [
+      {
+        petType: 'Dog',
+        planType: 'option1',
+        dob: nYearsAgo(5),
+      },
+      {
+        petType: 'Dog',
+        planType: 'option2',
+        dob: nYearsAgo(7),
+      },
+    ]
+
+    expect(pickAPrimary(pets).primaryPet).toEqual(pets[0])
+  })
+
+  test('It should pick the cat if other dogs are over 6', () => {
+    const pets = [
+      {
+        petType: 'Dog',
+        planType: 'option1',
+        dob: nYearsAgo(7),
+      },
+      {
+        petType: 'Cat',
+        planType: 'option1',
+        dob: nYearsAgo(7),
+      },
+      {
+        petType: 'Dog',
+        planType: 'option2',
+        dob: nYearsAgo(7),
+      },
+    ]
+
+    expect(pickAPrimary(pets).primaryPet).toEqual(pets[1])
   })
 })
